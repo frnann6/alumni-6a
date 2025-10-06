6b31191b0d2911f0f7df2ddd15fac666f5dd3937
FernandaDwi999 <fernandadchy@gmail.com>
2025-10-06 15:51:36 +0700

    move inline javascript in html

---
+++ alumni.js
@@ -0,0 +1,36 @@
+const mahasiswa = [
+  { id: 1, name: "Rifqi Efendi", instagram: "" },
+  { id: 2, name: "Anggita", instagram: "" },
+  { id: 3, name: "Aulia Salsabila", instagram: "aullia_aaa" },
+  { id: 4, name: "Aulia Rahmawati", instagram: "" },
+  { id: 5, name: "Ayu Radianti", instagram: "" },
+  { id: 6, name: "Celine Leisya", instagram: "celeisyaa_" },
+  { id: 7, name: "Dzikri Azharwan", instagram: "theazharone" },
+  { id: 8, name: "Elfira Fitriani", instagram: "ellfraaa" },
+  { id: 9, name: "Ersa", instagram: "" },
+  { id: 10, name: "Fadli Akbar", instagram: "one.fdzl" },
+  { id: 11, name: "Fernanda Dwi Cahaya", instagram: "frnann6" },
+  { id: 12, name: "Hasna Nur Azizah", instagram: "hasnanrzh" },
+  { id: 13, name: "Mei Dina", instagram: "anatsy.aa_" },
+  { id: 14, name: "Muhammad Nauval Rivaldi", instagram: "mnauval_rivaldi" },
+  { id: 15, name: "Moch Abdul Rizal", instagram: "abdulrizallll_" },
+  { id: 16, name: "Muhammad Fasya Ishaq", instagram: "planetafsy" },
+  { id: 17, name: "Rasya Aulia", instagram: "rasya_app" },
+  { id: 18, name: "Rendi Ramdani", instagram: "rendungg" },
+  { id: 19, name: "Salsa Ardhya Putri", instagram: "salssaardh" },
+  { id: 20, name: "Sanjani Anggara Putra", instagram: "" },
+  { id: 21, name: "Tia", instagram: "" },
+  { id: 22, name: "Tio Eka Putra", instagram: "___theyoe" },
+  { id: 23, name: "Yasrifa Azmalia Hildayanti", instagram: "yasrifaazmal" },
+  { id: 24, name: "Diva", instagram: "" },
+  { id: 25, name: "Amanda Putri", instagram: "amandaputri.13" },
+  { id: 26, name: "Daffa Ramdani", instagram: "dffa_rmdhni" },
+  { id: 27, name: "Yuri Arrizqi", instagram: "qizyur" },
+  { id: 28, name: "Rajib", instagram: "" },
+];
+
+function start() {
+  return {
+    items: mahasiswa,
+  };
+}
